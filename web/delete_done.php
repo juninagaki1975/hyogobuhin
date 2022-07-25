a7a3bab59344162bc01fbfcc298528bf37288474
juninagaki1975 <juninagaki1975@gmail.com>
2022-07-25 17:50:13 +0800

    submit.

--- web/delete_done.php
+++ web/delete_done.php
@@ -22,36 +22,36 @@ try{
     $msg = $e->getMessage();
 }
 
-$to = "juninagaki1975@gmail.com";
-$title = "[削除] ID: ".$data['id']." ".date("Y-m-d H:i:s");
-$mail_text = "下記のデータが削除されました<br/><br/>ID: ".$data['id']."<br/>案件番号:　".$data['anken_id']."<br/>件名: ".$data['item_name'];
+// $to = "juninagaki1975@gmail.com";
+// $title = "[削除] ID: ".$data['id']." ".date("Y-m-d H:i:s");
+// $mail_text = "下記のデータが削除されました<br/><br/>ID: ".$data['id']."<br/>案件番号:　".$data['anken_id']."<br/>件名: ".$data['item_name'];
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => "",
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"$to\"}],\"title\":\"$title\",\"html\":\"$mail_text\"}",
-    CURLOPT_HTTPHEADER => array(
-        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
-        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
-        "content-type: application/json"
-    )
-));
+// $curl = curl_init();
+// curl_setopt_array($curl, array(
+//     CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+//     CURLOPT_RETURNTRANSFER => true,
+//     CURLOPT_ENCODING => "",
+//     CURLOPT_MAXREDIRS => 10,
+//     CURLOPT_TIMEOUT => 0,
+//     CURLOPT_FOLLOWLOCATION => true,
+//     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//     CURLOPT_CUSTOMREQUEST => "POST",
+//     CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"$to\"}],\"title\":\"$title\",\"html\":\"$mail_text\"}",
+//     CURLOPT_HTTPHEADER => array(
+//         "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+//         "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+//         "content-type: application/json"
+//     )
+// ));
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
-curl_close($curl);
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
-}
+// $response = curl_exec($curl);
+// $err = curl_error($curl);
+// curl_close($curl);
+// if ($err) {
+//     echo "cURL Error #:" . $err;
+// } else {
+//     echo $response;
+// }
 
 ?>
 
