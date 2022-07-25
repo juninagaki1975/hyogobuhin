4e17f0b07bc71ec18c1d49d0023b745e5ee1c9a3
juninagaki1975 <juninagaki1975@gmail.com>
2022-07-25 09:50:18 +0800

    mail.

--- web/delete_done.php
+++ web/delete_done.php
@@ -30,6 +30,33 @@ $message = "以下のデータが削除されました：";
 $headers = "From: juninagaki1975@gmail.com";
 mb_send_mail($to, $subject, $message, $headers);
 
+$curl = curl_init();
+curl_setopt_array($curl, array(
+    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"$to\"}],\"title\":\"Title\",\"html\":\"Body\"}",
+    CURLOPT_HTTPHEADER => array(
+        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+        "content-type: application/json"
+    )
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+    echo "cURL Error #:" . $err;
+} else {
+    echo $response;
+}
+
 ?>
 
 <!DOCTYPE html>
