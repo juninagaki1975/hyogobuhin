c5b31d1d720de45c14c36e1797a97786607951ff
juninagaki1975 <juninagaki1975@gmail.com>
2022-07-28 16:15:54 +0800

    bootstrap.

--- web/delete_confirm.php
+++ web/delete_confirm.php
@@ -14,14 +14,6 @@ $sql = "SELECT * FROM anken WHERE id=".$id;
 $stmh = $pdo -> query($sql);
 $data = $stmh -> fetch();
 
-// try{
-//     $sql = "delete from anken where id=".$id;
-//     $stmt = $pdo -> query($sql);
-//     $stmt -> execute();
-// } catch (PDOException $e) { 
-//     $msg = $e->getMessage();
-// }
-
 ?>
 
 <!DOCTYPE html>
@@ -30,12 +22,21 @@ $data = $stmh -> fetch();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>データ削除</title>
 </head>
 <body>
-    <h2>データ削除</h2>
-    <?php echo date("Y-m-d"); ?>
-    <hr>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a href="#" class="navbar-brand mx-2">案件削除</a>
+        <div class="navbar-nav">
+            <a class="nav-item nav-link active" href="main.php">HOME</a>
+        </div>
+        <div class="navbar-nav col-md justify-content-end mx-2">
+            <a class="nav-item nav-link active" href="index.php">ログアウト</a>
+        </div>
+    </nav>
+
+    <div class="mx-3 my-3">
     <p>以下のデータを削除します</p>
     <table>
             <tr>
@@ -57,9 +58,8 @@ $data = $stmh -> fetch();
                 </td>
 
             </tr>
+            <tr><td><br></td></tr>
             <tr>
-                <td></td>
-                <td></td>
                 <td>
                     <form action="delete_done.php" method="POST">
                         <button type="submit" name="btn" value = <?php echo $id; ?> >決定</button>
@@ -67,12 +67,13 @@ $data = $stmh -> fetch();
                 </td>
             </tr>
         </table>
-   
+    </div>
 </body>
-<footer>
-    <br>
-    <a href="main.php">タイトルに戻る</a>
-    <hr>
 
-</footer>
+<div class="container">
+    <footer class="py-3 my-4">
+        <hr>
+        <p class="text-center text-muted">&copy; 2022 株式会社兵庫部品</p>
+  </footer>
+</div>
 </html>
